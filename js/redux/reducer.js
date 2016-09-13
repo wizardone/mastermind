4ae4060407cf1e3612827f625b2e05bd378595bb
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2016-09-13 21:49:56 +0300

    start implementing redux for store services

---
+++ js/redux/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  totalTurns: 8,
+  currentTurn: 0,
+  currentTurnCombination: null,
+  selectedPeg: null,
+  winningCombination: null
+}
+
+let mastermind = (state = initialState, action) => {
+  return state
+}
+
+module.exports = mastermind
