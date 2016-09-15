466d26ec8bd47ba4e00d5dff18676b824db4c916
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2016-09-15 10:57:01 +0300

    implement basic redux store

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import MainBoard from './components/main_board.js';
 import React from 'react';
 import ReactDom from 'react-dom';
+//import mastermind from './redux/reducer.js'
 
 ReactDom.render(<MainBoard />, document.getElementById('mastermind'));
--- js/redux/reducer.js
+++ js/redux/reducer.js
@@ -1,13 +1,28 @@
-const initialState = {
+let setWinningCombination = () => {
+  let winningCombination = [];
+  [...Array(4).keys()].map((i) => {
+    let freeColors = ['red', 'blue', 'yellow', 'green', 'pink', 'gray']
+    let color = freeColors[Math.floor(Math.random() * freeColors.length)]
+    winningCombination.push(color)
+  })
+  return winningCombination
+}
+
+export const initialState = {
   totalTurns: 8,
   currentTurn: 0,
   currentTurnCombination: null,
   selectedPeg: null,
-  winningCombination: null
+  winningCombination: setWinningCombination()
 }
 
-let mastermind = (state = initialState, action) => {
-  return state
+export let mastermind = (state, action) => {
+  switch (action.type) {
+    case 'SET_WINNING_COMBINATION':
+      return Object.assign({}, state, {
+        winningCombination: action.winningCombination
+      })
+    default:
+      return state
+  }
 }
-
-module.exports = mastermind
